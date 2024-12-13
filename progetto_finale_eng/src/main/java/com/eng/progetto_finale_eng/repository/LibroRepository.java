959c473e75527c99b498e6dfb27d96d16f56ee4b
ivanpak <piacere.ivan@gmail.com>
2024-12-13 15:56:03 +0100

    added repository e utente service

--- progetto_finale_eng/src/main/java/com/eng/progetto_finale_eng/repository/LibroRepository.java
+++ progetto_finale_eng/src/main/java/com/eng/progetto_finale_eng/repository/LibroRepository.java
@@ -1,9 +1,10 @@
 package com.eng.progetto_finale_eng.repository;
 
-import com.eng.progetto_finale_eng.model.Utente;
+import com.eng.progetto_finale_eng.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+
 }
